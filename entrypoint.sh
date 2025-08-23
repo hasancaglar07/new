21b87ebddb9e68dc9f5d426eeb77ab6be7dae74c
hasan caglar <bubutiko13@gmail.com>
2025-08-23 16:08:47 +0300

    Dosyalar güncellendi: app.py ve style.css değişiklikleriwqsadsadasdeq

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,4 @@ fi
 
 # Adım 2: Gunicorn sunucusunu başlat.
 echo "Starting Gunicorn server..."
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 --timeout 300
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 300
